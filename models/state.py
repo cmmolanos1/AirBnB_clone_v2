df67c6273c6a0af99bc851db77f96c14fd349a8e
Carlos Molano <926@holbertonschool.com>
2019-12-15 12:47:54 -0500

    Updating

--- models/state.py
+++ models/state.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """
+        returns the list of City instances with state_id equals
+        to the current State.id
+        """
+        list_city = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                list_city.append(city)
+        return list_city
