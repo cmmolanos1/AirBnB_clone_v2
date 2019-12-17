290311e23d5d57c4ec8a1d4490f0947c6d70bab8
alzheimeer <fogniebla@hotmail.com>
2019-12-17 11:45:42 -0500

    fix cascade city and user

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
@@ -17,4 +17,4 @@ class City(BaseModel, Base):
 
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    places = relationship('Place', backref='cities')
+    places = relationship('Place', cascade='all, delete', backref='cities')
