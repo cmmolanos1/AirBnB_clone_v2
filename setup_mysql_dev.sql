bf929a0e8c4cc0454c71a60d9d88f46639e9ced6
Carlos Molano <926@holbertonschool.com>
2019-12-17 14:44:05 -0500

    erasing

--- setup_mysql_dev.sql
+++
@@ -1,6 +0,0 @@
--- Creates the database hbnb_dev_db with specified paramenters
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++
@@ -1,6 +0,0 @@
--- Creates the database hbnb_test_db with specified paramenters
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
