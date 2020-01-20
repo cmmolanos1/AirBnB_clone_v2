d946fd803790271d2ba116b1540e687529659a9e
Carlos Molano <926@holbertonschool.com>
2020-01-20 09:01:32 -0500

    Upload 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Shows a message """
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ Print the message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ Print the message """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello_c(text):
+    """ Print the message """
+    a = text.replace('_', ' ')
+    return 'C {}'.format(a)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def hello_py(text='is fun'):
+    """ Print the message """
+    b = text.replace('_', ' ')
+    return 'Python {}'.format(b)
+
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    """ Print the number """
+    if type(n) is int:
+        return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """ Print the number """
+    if type(n) is int:
+        return render_template('5-number.html', number=n)
+
+if __name__ == '__main__':
+    app.run()
