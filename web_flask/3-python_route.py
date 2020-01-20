08baf4ea10b89781541b614f0ba5cf0da48ed1ce
Carlos Molano <926@holbertonschool.com>
2020-01-20 08:28:36 -0500

    Upload 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Shows a message """
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ Print the message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ Print the message """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello_c(text):
+    """ Print the message """
+    a = text.replace('_', ' ')
+    return 'C {}'.format(a)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def hello_py(text='is fun'):
+    """ Print the message """
+    b = text.replace('_', ' ')
+    return 'Python {}'.format(b)
+
+if __name__ == '__main__':
+    app.run()
