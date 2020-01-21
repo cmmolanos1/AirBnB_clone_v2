d0cc03900428361055ad2d430c34d172b173b43e
Carlos Molano <926@holbertonschool.com>
2020-01-21 12:24:05 -0500

    Changing slashes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,10 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_world():
     """ Print the message """
     return 'Hello HBNB!'
